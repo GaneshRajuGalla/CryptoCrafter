13eced15062ecad033592b0c9a465bbebfae4b64
ganeshrajugalla <ganeshrajugalla@gmail.com>
2023-11-13 12:43:44 +0530

    Project Setup

---
+++ CryptoCrafter/CryptoCrafter/CryptoCrafterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCrafterApp.swift
+//  CryptoCrafter
+//
+//  Created by Ganesh on 13/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCrafterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CryptoCrafter/CryptoCrafter/Utilities/Extensions/Color.swift
@@ -0,0 +1,8 @@
+//
+//  Color.swift
+//  CryptoCrafter
+//
+//  Created by Ganesh on 13/11/23.
+//
+
+import Foundation
