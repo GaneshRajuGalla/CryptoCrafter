29a466784420daed4ac3953140c590b783bff8c5
ganeshrajugalla <ganeshrajugalla@gmail.com>
2023-11-13 12:44:01 +0530

    Setup

--- CryptoCrafter/CryptoCrafter/ContentView.swift
+++ CryptoCrafter/CryptoCrafter/ContentView.swift
@@ -9,13 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        ZStack(content: {
+            
+            // background
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            // Stack of Colors
+            VStack(spacing: 40,content: {
+               Text("Accent Color")
+                    .foregroundStyle(Color.theme.accent)
+                
+                Text("Green Color")
+                    .foregroundStyle(Color.theme.green)
+                
+                Text("Red Color")
+                    .foregroundStyle(Color.theme.red)
+                
+                Text("Secondary Text Color")
+                    .foregroundStyle(Color.theme.secondaryText)
+                
+            })
+        })
     }
 }
 
